4f059909d5a7077c6f19980f8b3941b287f19d03
Vladimir <mvladimirn1989@gmail.com>
2022-04-03 23:05:03 +0300

    Урок 1

---
+++ lesson1/Robot.java
@@ -0,0 +1,29 @@
+package lesson1;
+
+public class Robot implements Member {
+    private Integer runLim;
+    private Integer jumpLim;
+    private String name;
+
+    public Robot(Integer runLim, Integer jumpLim, String name) {
+        this.runLim = runLim;
+        this.jumpLim = jumpLim;
+        this.name = name;
+    }
+
+    @Override
+    public void run(Treadmill treadmill) {
+        if (runLim<treadmill.getLength()){
+            System.out.println(name + "не смог пробежать "+ runLim+ " метров");
+        }
+        System.out.println(name+" пробежал "+ runLim+ " метров");
+    }
+
+    @Override
+    public void jump(Wall wall) {
+        if (jumpLim<wall.getHeight()){
+            System.out.println(name +" не прыгнул "+jumpLim + " сантиметров");
+        }
+        System.out.println(name+ " перепрыгнул "+jumpLim + " сантиметров");
+    }
+}
