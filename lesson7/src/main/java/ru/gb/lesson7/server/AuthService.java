507421ad910b9c2cdda8639836abe8a9465b7196
Vladimir <mvladimirn1989@gmail.com>
2022-05-12 21:59:27 +0300

    Урок 7

---
+++ lesson7/src/main/java/ru/gb/lesson7/server/AuthService.java
@@ -0,0 +1,11 @@
+package ru.gb.lesson7.server;
+
+import java.io.Closeable;
+
+public interface AuthService extends Closeable {
+
+    String getNickByLoginAndPassword(String login,String password);
+
+    void start();
+    void close();
+}
---
+++ lesson7/src/main/java/ru/gb/lesson7/server/ChatRunner.java
@@ -0,0 +1,11 @@
+package ru.gb.lesson7.server;
+
+
+//Launcher
+public class ChatRunner {
+    public static void main(String[] args) {
+        final ChatServer server = new ChatServer();
+    server.run();
+    }
+
+}
